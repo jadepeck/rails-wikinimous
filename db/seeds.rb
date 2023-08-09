e5db3be8e7d3423c0c54b9c5dcb8147c0acc3be9
pj taron <jadepeck27@gmail.com>
2023-08-09 11:03:18 +0900

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "start seeding"
+10.times do |a|
+  Article.create!(title: Faker::Book.title, content: Faker::Book.genre)
+end
+puts "finish seeding"
